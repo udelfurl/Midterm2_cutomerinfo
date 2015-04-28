bbea9acee7ff7208c9c3bcc31982d38f8251989d
Robert Furlong <stefurlr@udel.edu>
2015-04-28 15:11:16 -0400

    customer information

---
+++ src/ch/makery/Customer/Customer.java
@@ -0,0 +1,21 @@
+package ch.makery.Customer;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+public class Customer {
+	private final StringProperty firstname;
+	private final StringProperty MiddleInitial;
+	private final StringProperty lastname;
+	private final StringProperty address;
+	private final StringProperty city;
+	private final StringProperty zip;
+	private final RadioProperty male;
+	private final RadioProperty female;
+	
+	
+	
+
+}
---
+++ src/ch/makery/view/InformationController.java
@@ -0,0 +1,19 @@
+package ch.makery.view;
+
+public class InformationController {
+	Save.setOnAction(new EventHandler<ActionEvent>(){
+		private void handleOK(){
+			if(Action >=0){
+				Save Anchorview
+				}
+			}
+			
+			}
+		Clear.setOnAction(new EventHandler<ActionEvent>(){
+			private void handleHelp(){
+				if(Action >= 0){
+					Clear Anchorview
+					
+				}
+
+}
